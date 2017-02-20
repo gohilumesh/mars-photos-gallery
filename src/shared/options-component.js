56f1dec8c562cd5ce9e021465c35f5ebbc10510f
Umesh Gohil <gohilumesh@hotmail.com>
2017-02-19 18:22:10 -0800

    Updated

--- src/shared/options-component.js
+++ src/shared/options-component.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const OptionsComponent = ({options, isOpen, onSelect}) => {
   return (
-    <ul className="options-dropdown" className={isOpen ? 'active' : ''}>
+    <ul className={isOpen ? 'options-dropdown active' : 'options-dropdown'}>
     {
       options.map((option, idx) =>
         <li index={idx} value={option.value} key={option.id} onClick={onSelect}>
